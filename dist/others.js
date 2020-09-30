21fb77207df7da9e213312d1a413105c0339bd21
Julio Augusto <julioams@icloud.com>
2020-09-30 19:30:17 -0300

    add dist for test

---
+++ dist/others.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.emailValidation = void 0;
+// Email validation
+exports.emailValidation = (email) => {
+    let reg = /^[a-z][_a-z0-9\-.]+@[a-z0-9]+\.[a-z]+(\.[a-z]+)?$/i;
+    return reg.test(email);
+};
