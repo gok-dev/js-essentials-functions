2504edf8a65315155682cc9d8a0847be6c7e2fc9
Julio Augusto <julioams@icloud.com>
2020-10-10 10:36:17 -0300

    add coverage test config and github actions config

--- dist/cpf-cnpj.js
+++ dist/cpf-cnpj.js
@@ -27,23 +27,24 @@ exports.cpfCnpjUnmask = cpfCnpjUnmask;
 function cpfValidator(value) {
     let numbers, digits, sum, i, result, equal_digits;
     equal_digits = 1;
-    if (value.length < 11)
+    let cpf = value.replace(/\D/g, '');
+    if (cpf.length < 11)
         return false;
-    for (i = 0; i < value.length - 1; i++)
-        if (value.charAt(i) != value.charAt(i + 1)) {
+    for (i = 0; i < cpf.length - 1; i++)
+        if (cpf.charAt(i) != cpf.charAt(i + 1)) {
             equal_digits = 0;
             break;
         }
     if (!equal_digits) {
-        numbers = value.substring(0, 9);
-        digits = value.substring(9);
+        numbers = cpf.substring(0, 9);
+        digits = cpf.substring(9);
         sum = 0;
         for (i = 10; i > 1; i--)
             sum += numbers.charAt(10 - i) * i;
         result = sum % 11 < 2 ? 0 : 11 - (sum % 11);
         if (result != digits.charAt(0))
             return false;
-        numbers = value.substring(0, 10);
+        numbers = cpf.substring(0, 10);
         sum = 0;
         for (i = 11; i > 1; i--)
             sum += numbers.charAt(11 - i) * i;
