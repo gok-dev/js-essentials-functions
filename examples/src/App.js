35a18d1d289c86df8622023901f3f4a8b1986c9a
Guilherme <guilhermeht.ramos@gmail.com>
2020-11-03 22:54:12 -0300

    feat: adicionando exemplos em inglês e português

--- examples/src/App.js
+++ examples/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import {
   cpfCnpjMask,
   cpfCnpjUnmask,
@@ -26,6 +26,70 @@ function App() {
   const [cep, setCep] = useState('')
   const [datePt, setDatePt] = useState('')
   const [dateEn, setDateEn] = useState('')
+  const [language, setLanguage] = useState('ptbr')
+
+  const content = {
+    ptbr: {
+      selectLanguage: "Selecione seu idioma",
+      cpfCnpjMask: {
+        title: 'Máscara de CPF e CPF',
+        placeholderCPF: 'Informe seu CPF',
+        placeholderCNPJ: 'Informe seu CNPJ',
+        otherLabel: 'Limpar máscara'
+      },
+      cepMask: {
+        title: 'Máscara CEP',
+        placeholder: 'Informe seu CEP',
+        otherLabel: 'Limpar máscara'
+      },
+      dateMask: {
+        title: "Máscara de Data",
+        labelPtBr: 'Data PT-BR',
+        otherLabelPtBr: 'Padrão brasileiro: DD/MM/YYYY',
+        labelEn: 'Data EN-US',
+        otherLabelEn: 'Padrão americano: YYYY-MM-DD',
+        placeholder: 'Informe uma Data',
+      }
+    },
+    en: {
+      selectLanguage: "Select your language",
+      cpfCnpjMask: {
+        title: 'CPF e CPF Mask',
+        placeholderCPF: 'Your CPF',
+        placeholderCNPJ: 'Your CNPJ',
+        otherLabel: 'Clear mask'
+      },
+      cepMask: {
+        title: 'CEP Mask',
+        placeholder: 'Your CEP',
+        otherLabel: 'Clear mask'
+      },
+      dateMask: {
+        title: "Date Mask",
+        labelPtBr: 'Date PT-BR',
+        otherLabelPtBr: 'Brazilian format: DD/MM/YYYY',
+        labelEn: 'Date EN-US',
+        otherLabelEn: 'American format: YYYY-MM-DD',
+        placeholder: 'Date',
+      }
+    },
+  }
+
+  const handleLanguageSelection = (lang) => {
+    localStorage.setItem('@JsEssentialsFunctionsLanguage', lang);
+    setLanguage(lang)
+  }
+
+  useEffect(() => {
+    const storageLanguage = localStorage.getItem('@JsEssentialsFunctionsLanguage');
+
+    if(storageLanguage && storageLanguage === 'en') {
+      handleLanguageSelection('en')
+    } else {
+      handleLanguageSelection('ptbr')
+    }
+  }, [])
+
 
   return (
     <Container
@@ -35,9 +99,19 @@ function App() {
         padding: '50px 0',
       }}
     >
+      <div className="selectLanguage">
+        <span>{content[language].selectLanguage}: </span>
+        <button onClick={() => handleLanguageSelection('ptbr')}>
+          <span role="img" aria-label="Brandeira do Brasil">🇧🇷</span>
+        </button>
+        <span> / </span>
+        <button onClick={() => handleLanguageSelection('en')}>
+          <span role="img" aria-label="United States flag">🇺🇸</span>
+        </button>
+      </div>
       <Card>
         <CardHeader>
-          <h1>CPF and CNPJ Mask</h1>
+          <h1>{content[language].cpfCnpjMask.title}</h1>
         </CardHeader>
         <CardBody>
           <Form>
@@ -47,10 +121,10 @@ function App() {
               name="cpf"
               id="cpf"
               maxLength={15}
-              placeholder="Informe seu CPF"
+              placeholder={content[language].cpfCnpjMask.placeholderCPF}
               onChange={(e) => setCpf(e.target.value)}
               value={cpfCnpjMask(cpf)}
-              otherLabel="Clear Mask:"
+              otherLabel={`${content[language].cpfCnpjMask.otherLabel}:`}
               secondFunc={cpfCnpjUnmask(cpf)}
             />
             <InputGroup
@@ -59,10 +133,10 @@ function App() {
               name="cnpj"
               id="cnpj"
               maxLength={18}
-              placeholder="Informe seu CNPJ"
+              placeholder={content[language].cpfCnpjMask.placeholderCNPJ}
               onChange={(e) => setCnpj(e.target.value)}
               value={cpfCnpjMask(cnpj)}
-              otherLabel="Clear Mask:"
+              otherLabel={`${content[language].cpfCnpjMask.otherLabel}:`}
               secondFunc={cpfCnpjUnmask(cnpj)}
             />
           </Form>
@@ -71,7 +145,7 @@ function App() {
       <hr />
       <Card>
         <CardHeader>
-          <h1>CEP Mask</h1>
+        <h1>{content[language].cepMask.title}</h1>
         </CardHeader>
         <CardBody>
           <Form>
@@ -81,10 +155,10 @@ function App() {
               name="cep"
               id="cep"
               maxLength={9}
-              placeholder="Informe seu CEP"
+              placeholder={content[language].cepMask.placeholder}
               onChange={(e) => setCep(e.target.value)}
               value={cepMask(cep)}
-              otherLabel="Clear Mask:"
+              otherLabel={content[language].cepMask.otherLabel}
               secondFunc={cepUnmask(cep)}
             />
           </Form>
@@ -93,31 +167,31 @@ function App() {
       <hr />
       <Card>
         <CardHeader>
-          <h1>Date Mask</h1>
+          <h1>{content[language].dateMask.title}</h1>
         </CardHeader>
         <CardBody>
           <Form>
             <InputGroup
-              label="DATE PT-BR"
+              label={content[language].dateMask.labelPtBr}
               type="text"
               name="date-br"
               id="date-br"
               maxLength={10}
-              placeholder="Informe uma Data"
+              placeholder={content[language].dateMask.placeholder}
               onChange={(e) => setDatePt(e.target.value)}
               value={dateMask(datePt)}
-              otherLabel="Padrão Brasileiro: DD/MM/YYYY"
+              otherLabel={content[language].dateMask.otherLabelPtBr}
             />
             <InputGroup
-              label="DATE EN-US"
+              label={content[language].dateMask.labelEn}
               type="text"
               name="date-en"
               id="date-en"
               maxLength={10}
-              placeholder="Informe uma Data"
+              placeholder={content[language].dateMask.placeholder}
               onChange={(e) => setDateEn(e.target.value)}
               value={dateMaskUS(dateEn)}
-              otherLabel="Padrão Americano: YYYY-MM-DD"
+              otherLabel={content[language].dateMask.otherLabelEn}
             />
           </Form>
         </CardBody>
