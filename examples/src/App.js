eef4b0e01bd80960f37943ef269f8ea60385e535
Guilherme <guilhermeht.ramos@gmail.com>
2020-11-03 21:57:40 -0300

    feat: criando função e preparando o examples

--- examples/src/App.js
+++ examples/src/App.js
@@ -5,7 +5,8 @@ import {
   cepMask,
   cepUnmask,
   dateMask,
-  dateMaskUS
+  dateMaskUS,
+
 } from 'js-essentials-functions'
 import './App.css'
 
@@ -118,6 +119,28 @@ function App() {
               onChange={(e) => setDateEn(e.target.value)}
               value={dateMaskUS(dateEn)}
               otherLabel="Padrão Americano: YYYY-MM-DD"
+            />
+          </Form>
+        </CardBody>
+      </Card>
+      <hr />
+      <Card>
+        <CardHeader>
+          <h1>GET AGE</h1>
+        </CardHeader>
+        <CardBody>
+          <Form>
+            <InputGroup
+              label="BIRTHDAY"
+              type="text"
+              name="birthday"
+              id="birthday"
+              maxLength={10}
+              placeholder="Informe a data de nascimento"
+              onChange={(e) => setDatePt(e.target.value)}
+              value={dateMask(datePt)}
+              otherLabel={`Age: `}
+              secondFunc={'21'}
             />
           </Form>
         </CardBody>
